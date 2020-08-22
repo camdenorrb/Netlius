cbf1d54bb4d883eade570afb1fefe0c527337ea6
camdenorrb <camdenorrb@users.noreply.github.com>
2020-08-22 05:48:37 -0500

    Remove atomicfu reliance in actual program.

--- src/main/kotlin/me/camdenorrb/netlius/Main.kt
+++ src/main/kotlin/me/camdenorrb/netlius/Main.kt
@@ -1,8 +1,6 @@
 package me.camdenorrb.netlius
 
-import kotlinx.atomicfu.atomic
 import kotlinx.coroutines.*
-import me.camdenorrb.netlius.net.Packet
 
 // This project should essentially be a bridge between the driver and JVM along with some coroutines
 // TODO: Could potentially use https://github.com/rambodrahmani/linux-pspat for now
@@ -10,37 +8,6 @@ object Main {
 
     @JvmStatic
     fun main(args: Array<String>): Unit = runBlocking {
-
-        val server = Netlius.server("127.0.0.1", 25565)
-
-        server.onConnect {
-            while (channel.isOpen) {
-                println(readString())
-            }
-        }
-
-        val count = atomic(0)
-
-        (0..100).map {
-            async(Netlius.threadPoolDispatcher) {
-                val client = Netlius.clientSuspending("127.0.0.1", 25565)
-                client.queueAndFlush(Packet().string("Test${count.getAndAdd(1)}"))
-            }
-        }.awaitAll()
-
-
-        //println(DirectByteBufferPool.byteBuffers.firstEntry().value.size)
-
-        delay(1000)
-
-
-        println(count)
-
-        server.stop()
-        Netlius.stop()
-
-        //println(server.clients.size)
-        //println(server.clients.firstOrNull()?.channel?.isOpen)
 
     }
 
--- src/test/kotlin/me/camdenorrb/netlius/ClientTest.kt
+++ src/test/kotlin/me/camdenorrb/netlius/ClientTest.kt
@@ -1,9 +1,47 @@
 package me.camdenorrb.netlius
 
+import kotlinx.atomicfu.atomic
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import me.camdenorrb.netlius.net.Packet
 import kotlin.test.Test
 
 class ClientTest {
+
+    @Test
+    fun `attack of the clients part 2 season 4`() = runBlocking {
+
+        val server = Netlius.server("127.0.0.1", 25565)
+
+        server.onConnect {
+            while (channel.isOpen) {
+                println(readString())
+            }
+        }
+
+        val count = atomic(0)
+
+        (0..100).map {
+            async(Netlius.threadPoolDispatcher) {
+                val client = Netlius.clientSuspending("127.0.0.1", 25565)
+                client.queueAndFlush(Packet().string("Test${count.getAndAdd(1)}"))
+            }
+        }.awaitAll()
+
+
+        //println(DirectByteBufferPool.byteBuffers.firstEntry().value.size)
+
+        delay(1000)
+
+
+        assert(count.value == 101)
+
+        server.stop()
+        Netlius.stop()
+
+    }
 
     // Takes 30 seconds to run due to timeout
     @Test
